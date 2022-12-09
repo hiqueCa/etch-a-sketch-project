e4301988ff56a587e3023c947fdcd5cb96dddc79
Henrique Caltram <henrivai@hotmail.com>
2022-12-09 09:03:45 -0300

    Add the same grid-item to all grid cells and style it properly

--- logic.js
+++ logic.js
@@ -5,11 +5,10 @@ const gridContainer = document.querySelector(".grid");
 gridContainer.style.setProperty('grid-template-columns', `repeat(${gridXDivisions}, 1fr)`);
 gridContainer.style.setProperty('grid-template-rows', `repeat(${gridYDivisions}, 1fr`);
 
-const gridItem = document.createElement("div");
-gridItem.className = "grid-item";
-
 const gridItemsNumber = gridXDivisions * gridYDivisions;
 
 for (var i = 0; i < gridItemsNumber; i++) {
+  const gridItem = document.createElement("div");
+  gridItem.className = "grid-item";
   gridContainer.appendChild(gridItem);
 };
