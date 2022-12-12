d861963781cf1d9dab3a49dbf5183a93150cf5fa
Henrique Caltram <henrivai@hotmail.com>
2022-12-12 12:39:44 -0300

    Simplify number of items constant so that grid is always square

--- logic.js
+++ logic.js
@@ -1,11 +1,9 @@
-const gridXDivisions = 16;
-const gridYDivisions = 16;
+const gridItemsPerRow = 16;
+const gridItemsNumber = gridItemsPerRow ** 2;
 
 const gridContainer = document.querySelector(".grid");
 gridContainer.style.setProperty('grid-template-columns', `repeat(${gridXDivisions}, 1fr)`);
 gridContainer.style.setProperty('grid-template-rows', `repeat(${gridYDivisions}, 1fr`);
-
-const gridItemsNumber = gridXDivisions * gridYDivisions;
 
 for (var i = 0; i < gridItemsNumber; i++) {
   const gridItem = document.createElement("div");
