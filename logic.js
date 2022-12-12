e3e9d30ffa4e88d702d7b3e90910b0aee8400ca7
Henrique Caltram <henrivai@hotmail.com>
2022-12-12 12:42:17 -0300

    Replace renamed constants names

--- logic.js
+++ logic.js
@@ -2,8 +2,8 @@ const gridItemsPerRow = 16;
 const gridItemsNumber = gridItemsPerRow ** 2;
 
 const gridContainer = document.querySelector(".grid");
-gridContainer.style.setProperty('grid-template-columns', `repeat(${gridXDivisions}, 1fr)`);
-gridContainer.style.setProperty('grid-template-rows', `repeat(${gridYDivisions}, 1fr`);
+gridContainer.style.setProperty('grid-template-columns', `repeat(${gridItemsPerRow}, 1fr)`);
+gridContainer.style.setProperty('grid-template-rows', `repeat(${gridItemsPerRow}, 1fr`);
 
 for (var i = 0; i < gridItemsNumber; i++) {
   const gridItem = document.createElement("div");
