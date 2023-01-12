daea665bc62c1a80c3bf6aebda18f02de10c0722
Henrique Caltram <henrivai@hotmail.com>
2023-01-12 15:03:50 -0300

    Make grid items colourable and add new dimensions button

--- logic.js
+++ logic.js
@@ -1,26 +1,40 @@
-const gridItemsPerRow = 16;
-const gridItemsNumber = gridItemsPerRow ** 2;
-
 const grid = document.querySelector(".grid");
 const gridHeight = grid.clientHeight;
 
-const gridItemDimension = `${gridHeight / gridItemsPerRow}px`;
-
-grid.style.setProperty('grid-template-columns', `repeat(${gridItemsPerRow}, ${gridItemDimension})`);
-grid.style.setProperty('grid-template-rows', `repeat(${gridItemsPerRow}, ${gridItemDimension}`);
-
-for (var i = 0; i < gridItemsNumber; i++) {
-  const gridItem = document.createElement("div");
-  gridItem.className = "grid-item";
-  gridItem.style.setProperty('width', gridItemDimension);
-  gridItem.style.setProperty('height', gridItemDimension);
-  grid.appendChild(gridItem);
+const makeGridItemsColourable = () => {
+  const gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach(gridItem => {
+    gridItem.addEventListener(
+      'mouseover', 
+      e => e.target.style.backgroundColor = 'black',
+    );
+  });
 };
 
-const gridItems = document.querySelectorAll('.grid-item');
-gridItems.forEach(gridItem => {
-  gridItem.addEventListener(
-    'mouseover', 
-    e => e.target.style.setProperty('background-color', 'black')
-  );
-});
+const generateGrid = (numberOfGridItems) => {
+  const gridItemsPerRow = numberOfGridItems || 16;
+  const gridItemsNumber = gridItemsPerRow ** 2;
+  const gridItemDimension = `${gridHeight / gridItemsPerRow}px`;
+
+  grid.style.setProperty('grid-template-columns', `repeat(${gridItemsPerRow}, ${gridItemDimension})`);
+  grid.style.setProperty('grid-template-rows', `repeat(${gridItemsPerRow}, ${gridItemDimension}`);
+
+  for (var i = 0; i < gridItemsNumber; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.className = "grid-item";
+    gridItem.style.setProperty('width', gridItemDimension);
+    gridItem.style.setProperty('height', gridItemDimension);
+    grid.appendChild(gridItem);
+  };
+
+  makeGridItemsColourable();
+};
+
+const resetGrid = () => {
+  const gridItems = document.querySelectorAll('.grid-item');
+
+  const newNumberOfCells = prompt('Input a new number of cells per row:');
+  generateGrid(newNumberOfCells);
+}
+
+window.addEventListener('DOMContentLoaded', generateGrid());
